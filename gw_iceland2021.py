e75152482e180d329d32a5b329ff83d095202f01
cralvizuri <celso.alvizuri@gmail.com>
2021-07-27 18:34:33 +0200

    fix input options ev_info; fix rescale

--- gw_iceland2021.py
+++ gw_iceland2021.py
@@ -6,6 +6,7 @@ import sys
 def get_ev_info(ev_info,iex):
 # ===============================================================
     if iex == 0:
+        ev_info.overwrite_ddir = 0
         # DATA PREPARATION / PROCESSING
         ev_info.use_catalog = 0
         ev_info.ifmass_downloader = True
@@ -40,6 +41,7 @@ def get_ev_info(ev_info,iex):
 
         #ev_info.network = 'ZB'
         ev_info.channel = 'LH?,BH?,HH?'
+        ev_info.resample_TF = True
         ev_info.resample_freq = 20
         ev_info.scale_factor = 100
         #ev_info.phase_window = False
@@ -51,10 +53,11 @@ def get_ev_info(ev_info,iex):
         #------------------------------
 
     elif iex == 1:  # iterate list
+        ev_info.overwrite_ddir = 0
         # DATA PREPARATION / PROCESSING
         ev_info.use_catalog = 0
         ev_info.ifmass_downloader = True
-        ev_info.ifverbose = False    # output all proccessing steps
+        ev_info.ifverbose = True    # output all proccessing steps
 
         ev_info.icreateNull = 0
 
@@ -66,6 +69,7 @@ def get_ev_info(ev_info,iex):
         #-----------------------------------------------------------
         # BEGIN ITERATE EVENTS
         inputfile = 'iceland_quake_cat_01-18_Mge4p5'  # 2021-05-12
+        #inputfile = 'iceland_quake_cat_01-18_Mge4p5_try2'  # 2021-06-27
         # 2021-02-24T10:05:57.024 -22.20764 63.91658 1.098 5.72 5.13
         # 2021-02-24T10:17:53.076 -22.32224 63.89646 4.862 4.56 4.53
         # 2021-02-24T10:27:59.639 -22.05429 63.91842 3.827 4.70 4.30
@@ -81,8 +85,8 @@ def get_ev_info(ev_info,iex):
         ev_info_list = []
         for i, ievid in enumerate(otime):
             print('Processing event %d otime %s' %(i, ievid))
-            iev_info = ev_info.copy()
             # template
+            iev_info = ev_info.copy()
             iev_info.otime = obspy.UTCDateTime(otime[i])
             iev_info.elon = lon[i]
             iev_info.elat = lat[i]
@@ -90,19 +94,20 @@ def get_ev_info(ev_info,iex):
             iev_info.emag = mag[i]
 
             # same requests for all events
-            ev_info.min_dist = 0
-            ev_info.max_dist = 2000
-            ev_info.tbefore_sec = 100
-            ev_info.tafter_sec = 1000
-            ev_info.channel = 'LH?,BH?,HH?'
-            ev_info.resample_freq = 50
-            ev_info.scale_factor = 100
+            iev_info.min_dist = 0
+            iev_info.max_dist = 2000
+            iev_info.tbefore_sec = 100
+            iev_info.tafter_sec = 1000
+            iev_info.channel = 'BH?,HH?'
+            iev_info.resample_TF = True
+            iev_info.resample_freq = 20
+            iev_info.scale_factor = 100
 
             ev_info_list.append(iev_info)
         # END ITERATE EVENTS
         #-----------------------------------------------------------
 
         ev_info = ev_info_list
-        print(ev_info_list)
+        print(ev_info)
 
     return(ev_info)
