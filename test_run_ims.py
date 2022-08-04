6fc101c3840e17fc23d69441de45939b649ff59b
cralvizuri <celso.alvizuri@gmail.com>
2022-08-04 15:00:41 +0200

    add option to subset by radius

--- test_run_ims.py
+++ test_run_ims.py
@@ -20,17 +20,19 @@ path_to_nms_client = '/nobackup/celso/REPOSITORIES/IMS-nms_client3/nms_client3'
 print('Initializing seismonpy-IMS interface')
 from obspy import UTCDateTime
 from seismonpy.utils.ims_request import IMS_Client as Client
+from obspy.geodetics import kilometer2degrees
 import warnings
 
 warnings.filterwarnings('ignore')
 client = Client(nms_path=path_to_nms_client)
 #client = Client(nms_path=path_to_nms_client, test_nms=False)
-
-print('REQUESTING STATION METADATA ...')
 station = "USRK" # or multiple stations separated by comma. "SPA0,ARA0" 
-inventory=client.get_stations(station=station)
-print(inventory)
-#print(inventory[0][-1][0].response)
+station = "*" # or multiple stations separated by comma. "SPA0,ARA0" 
+
+#print('REQUESTING STATION INVENTORY ...')
+#inventory=client.get_stations(station=station)
+#print(inventory)
+##print(inventory[0][-1][0].response)
 
 #inventory=client.get_stations(station="hydroacoustic")
 #inventory.plot();
@@ -40,9 +42,10 @@ print('REQUESTING STATION INVENTORY ...')
 lon0, lat0 = 132.0, 44.2  # station USRK
 maxrad_km = 2
 lon0, lat0 = 129.0, 41.3  # NK test site
-maxrad_km = 500     # include USRK, KSRS
+maxrad_km = 2000     # include USRK, KSRS
+maxrad = kilometer2degrees(maxrad_km)
 #inventory=client.get_stations(station=station,use_cache=True)
-inventory=client.get_stations(station=station, longitude=lon0, latitude=lat0, maxradius=maxrad_km, use_cache=False)
+inventory=client.get_stations(station=station, longitude=lon0, latitude=lat0, maxradius=maxrad, use_cache=False)
 #inventory=client.get_stations(minlongitude=0.0,maxlongitude=20.0,minlatitude=50.0,maxlatitude=70.0,use_cache=True)
 #inventory=client.get_stations(station='seismic',minlongitude=0.0,maxlongitude=20.0,minlatitude=50.0,maxlatitude=70.0)
 print(inventory)
@@ -56,4 +59,3 @@ st.plot();
 print('REQUESTING EVENT METADATA ...')
 cat=client.get_events(starttime=UTCDateTime('2022-01-01T'),endtime=UTCDateTime('2022-06-30T'),minmagnitude=5.0,mindepth=20,maxdepth=100,minlongitude=0,maxlongitude=50)
 print(cat)
- 
