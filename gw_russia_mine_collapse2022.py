0d90bb9259ece09bb74a18e5b4175e957d8c4f0b
cralvizuri <celso.alvizuri@gmail.com>
2023-11-10 14:54:39 +0100

    add additional origin + option to process local data

--- gw_russia_mine_collapse2022.py
+++ gw_russia_mine_collapse2022.py
@@ -26,15 +26,16 @@ def get_ev_info(ev_info,iex):
 
         ## EVENT INFO USGS
         # ISC: http://www.isc.ac.uk/cgi-bin/web-db-run?event_id=622117251&out_format=ISF2&request=COMPREHENSIVE
-        # ISC, UPPSALA: 2022/03/05 00:13:26.10   2.50 0.500  67.6150   34.0440  0.13   0.5  -1   0.1f             11                       se UPP         616546394
+        # ISC, UPPSALA:  2022/03/05 00:13:26.10   2.50 0.500  67.6150   34.0440  0.13   0.5  -1   0.1f             11                       se UPP         616546394
+        # GSRAS, Russia: 2022-03-05 00:13:25    67.68   34.15   2   30  -   4.4 5   Baltic-Belarus-NW Russia reg.
         ev_info.otime = obspy.UTCDateTime("2022-03-05T00:13:26.1000") # obtained 2022-09-05 
         ev_info.elon = 34.0440
         ev_info.elat = 67.6150
         ev_info.edep = 0
         ev_info.emag = 3.1 # ML     3.1          UPP         616546394
 
-        ev_info.min_dist = 1500
-        ev_info.max_dist = 2200
+        ev_info.min_dist = 00
+        ev_info.max_dist = 2000
         ev_info.tbefore_sec = 100
         ev_info.tafter_sec = 2000
 
@@ -51,19 +52,25 @@ def get_ev_info(ev_info,iex):
         #------------------------------
 
         #-----------------------------------------------------------
-        # IMS REQUEST
-        # Be sure to specify each IMS station. wildcard '*' is not implemented.
-        ev_info.idb = 0
+        # PROCESS LOCAL FILES: client_name="local" self.ifmass_downloader=False
+        ev_info.client_name = "local"
         ev_info.ifmass_downloader = False
-        ev_info.client_name = 'IMS-SMP'
-        #ev_info.station = 'FINES'
-        #ev_info.station = 'I37NO'
-        #ev_info.station = 'HFS' # 2022-09-23 NOTE mismatch channel metadata. HH EH
-        ev_info.channel = '*H*' # DEFAULT. be sure to include SH for IMS data (vertical sensors)
-                                # do not use '*' bc it finds BDF LDA data with no instrument response.
-        #ev_info.channel = 'BH*,HH*,SH*,MH*' # 2022-09-12 doesnt' work.
-        ev_info.icreateNull = 1 # NOTE USE. Else vertical-components will not be saved.
         #-----------------------------------------------------------
+
+        ##-----------------------------------------------------------
+        ## IMS REQUEST
+        ## Be sure to specify each IMS station. wildcard '*' is not implemented.
+        #ev_info.idb = 0
+        #ev_info.ifmass_downloader = False
+        #ev_info.client_name = 'IMS-SMP'
+        ##ev_info.station = 'FINES'
+        ##ev_info.station = 'I37NO'
+        ##ev_info.station = 'HFS' # 2022-09-23 NOTE mismatch channel metadata. HH EH
+        #ev_info.channel = '*H*' # DEFAULT. be sure to include SH for IMS data (vertical sensors)
+        #                        # do not use '*' bc it finds BDF LDA data with no instrument response.
+        ##ev_info.channel = 'BH*,HH*,SH*,MH*' # 2022-09-12 doesnt' work.
+        #ev_info.icreateNull = 1 # NOTE USE. Else vertical-components will not be saved.
+        ##-----------------------------------------------------------
 
 
     return(ev_info)
